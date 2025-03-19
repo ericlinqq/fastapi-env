ad2f519c3c3a341f365c5ddcdfb3ad3d8feead6e
eric <ericlin66.cs11@nycu.edu.tw>
2025-03-19 22:01:59 +0800

    Add hello and feature endpoint

--- test/feature.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/feature")
-def read_feature():
-    return {"message": "Enhanced feature"}
--- test/main.py
+++ test/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/hello")
 def read_hello():
     return {"message": "Hello world"}
+
+
+@app.get("/feature")
+def read_feature():
+    return {"message": "New feature"}
